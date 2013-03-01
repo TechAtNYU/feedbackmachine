9db5a9d710a13aa713fdabdf28588947456d1cb3
mponizil <misha.ponizil@gmail.com>
2013-03-01 11:52:39 -0500

    Bring back mobile detection for fingerblast. Cleaner cache-busting of assets. Snip a print.

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.http import require_POST
 
 from app.models import Event, Demo, Comment
 
-import json, time
+import json
 
 
 def current_demo(request, event_id):
@@ -35,8 +35,7 @@ def index(request):
     return render(request, 'index.html', {
         'event': event,
         'current_demo': event.current_demo,
-        'demos': event.demos.all(),
-        'stamp': int(time.time())
+        'demos': event.demos.all()
     })
 
 
@@ -44,10 +43,8 @@ def demo(request, demo_id):
     demo = Demo.objects.get(id=demo_id)
     event = Event.objects.get(id=demo.event.id)
     current_demo = event.current_demo
-    print int(time.time())
     return render(request, 'demo.html', {
         'event': event,
         'demo': demo,
-        'current_demo': current_demo,
-        'stamp': int(time.time())
+        'current_demo': current_demo
     })
